ff3b9919e153b162c14913a249d23fde84771d5a
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-10-28 14:19:59 +0300

    modified loop

--- looping.rb
+++ looping.rb
@@ -8,7 +8,7 @@ def happy_new_year
   puts "Happy New Year!"
 end
 
-# No need to modify this code! Use this to implement the fizzbuzz_printer method.
+# No need to modify this code! Use this to implement the fizzbuzz_printer metd.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
